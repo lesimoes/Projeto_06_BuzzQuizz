498acc6f68e88d433f4a5083bbac375973a4d9b7
renaatosalgado <renaato.salgado@gmail.com>
2021-08-11 15:58:29 -0300

    added function showAllQuizzes

--- script.js
+++ script.js
@@ -10,6 +10,64 @@ function getAllQuizzes () {
 function loadAllQuizzes (response) {
     allQuizzes = response.data;
     console.log(allQuizzes);
+
+    showAllQuizzes();
+}
+
+function showAllQuizzes () {
+    const ulQuizzes = document.querySelector('.allQuizzes ul');
+    ulQuizzes.innerHTML = "";
+
+    for (let i = 0; i < allQuizzes.length; i++) {
+        ulQuizzes.innerHTML += `
+        <li>
+            <img src=${allQuizzes[i].image}>
+            <p class="quizzTitle">${allQuizzes[i].title}</p>
+        </li>
+        <li>
+        <img src=${allQuizzes[i].image}>
+        <p class="quizzTitle">${allQuizzes[i].title}</p>
+    </li>
+    <li>
+    <img src=${allQuizzes[i].image}>
+    <p class="quizzTitle">${allQuizzes[i].title}</p>
+</li><li>
+<img src=${allQuizzes[i].image}>
+<p class="quizzTitle">${allQuizzes[i].title}</p>
+</li>
+<li>
+<img src=${allQuizzes[i].image}>
+<p class="quizzTitle">${allQuizzes[i].title}</p>
+</li>
+<li>
+<img src=${allQuizzes[i].image}>
+<p class="quizzTitle">${allQuizzes[i].title}</p>
+</li>
+<li>
+            <img src=${allQuizzes[i].image}>
+            <p class="quizzTitle">${allQuizzes[i].title}</p>
+        </li>
+        <li>
+        <img src=${allQuizzes[i].image}>
+        <p class="quizzTitle">${allQuizzes[i].title}</p>
+    </li>
+    <li>
+    <img src=${allQuizzes[i].image}>
+    <p class="quizzTitle">${allQuizzes[i].title}</p>
+</li><li>
+<img src=${allQuizzes[i].image}>
+<p class="quizzTitle">${allQuizzes[i].title}</p>
+</li>
+<li>
+<img src=${allQuizzes[i].image}>
+<p class="quizzTitle">${allQuizzes[i].title}</p>
+</li>
+<li>
+<img src=${allQuizzes[i].image}>
+<p class="quizzTitle">${allQuizzes[i].title}</p>
+</li>`
+        
+    }
 }
 
 getAllQuizzes ();
